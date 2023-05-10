50a2445ba5bb9c862bf5b123384cd429dd920084
mrdevsam <mdshoykotislam05@gmail.com>
2023-05-10 08:15:42 +0000

    initial commit

---
+++ src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.mrdevsam.projects.example.springgraphqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrdevsam.projects.example.springgraphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
