3a711d20d2ea61e70aa714c8f53af20d27a3e404
mrdevsam <mdshoykotislam05@gmail.com>
2023-05-17 17:26:54 +0000

    adding data layer

--- src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
+++ src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
@@ -3,12 +3,14 @@ package dev.mrdevsam.projects.example.springgraphqldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.*;
-import org.springframework.graphql.data.method.annotation.*;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import jakarta.annotation.PostConstruct;
-import java.util.concurrent.atomic.AtomicInteger;
+import jakarta.persistence.*;
+import lombok.*;
+//import java.util.*;
+//import org.springframework.graphql.data.method.annotation.*;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Service;
+//import jakarta.annotation.PostConstruct;
+//import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class SpringGraphqlDemoApplication {
@@ -19,3 +21,40 @@ public class SpringGraphqlDemoApplication {
 
 }
 
+//Data layer
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+class Address {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+}
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+class Person {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
+	private Address address;
+}
