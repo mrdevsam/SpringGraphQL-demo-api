0d81bd77dc963035d46edaa4372883737fdc832c
mrdevsam <mdshoykotislam05@gmail.com>
2023-05-23 19:35:10 +0000

    adding data model and repository

--- src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
+++ src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
@@ -3,10 +3,15 @@ package dev.mrdevsam.projects.example.springgraphqldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.*;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import graphql.scalars.ExtendedScalars;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+import org.springframework.data.repository.ListCrudRepository;
 
 @SpringBootApplication
 public class SpringGraphqlDemoApplication {
@@ -28,3 +33,31 @@ class GraphqlConfig {
 								.scalar(ExtendedScalars.GraphQLBigDecimal);
 	}
 }
+
+// data models
+@Entity
+@NoArgsConstructor
+@Data
+class Product {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String title;
+	private Boolean isOnSale;
+	private Float weight;
+	private BigDecimal price;
+	private LocalDate creationDate;
+
+	public Product(String title, Boolean isOnSale, Float weight, BigDecimal price, LocalDate creationDate) {
+		this.title = title;
+		this.isOnSale = isOnSale;
+		this.weight = weight;
+		this.price = price;
+		this.creationDate = creationDate;
+	}
+	
+}
+
+//repository
+interface ProductRepo extends ListCrudRepository<Product, Integer> {}
