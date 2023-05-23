f233aadf6f29e7a7cd1c11ab7246bf04b0b7e123
mrdevsam <mdshoykotislam05@gmail.com>
2023-05-23 16:48:56 +0000

    adding configuration for graphql custom scalars

--- src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
+++ src/main/java/dev/mrdevsam/projects/example/springgraphqldemo/SpringGraphqlDemoApplication.java
@@ -3,6 +3,11 @@ package dev.mrdevsam.projects.example.springgraphqldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
 @SpringBootApplication
 public class SpringGraphqlDemoApplication {
 
@@ -10,4 +15,16 @@ public class SpringGraphqlDemoApplication {
 		SpringApplication.run(SpringGraphqlDemoApplication.class, args);
 	}
 
+}
+
+
+//configurations
+@Configuration
+class GraphqlConfig {
+
+	@Bean
+	public RuntimeWiringConfigurer configurer() {
+		return wiringBuilder -> wiringBuilder
+								.scalar(ExtendedScalars.GraphQLBigDecimal);
+	}
 }
